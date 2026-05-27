cd7a8c0ea6a83e1339c18b00aea910326279d88a
leonel-1202 <wilfredo.silval@uniagustiniana.edu.co>
2026-05-27 14:51:23 -0500

    change

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,19 +1,26 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth }       from "firebase/auth";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZdcW4g9apX8NtfG5EHSsRtwC4ogg0BEs",
-  authDomain: "nexus-1f56f.firebaseapp.com",
-  projectId: "nexus-1f56f",
-  storageBucket: "nexus-1f56f.firebasestorage.app",
+  apiKey:            "AIzaSyAZdcW4g9apX8NtfG5EHSsRtwC4ogg0BEs",
+  authDomain:        "nexus-1f56f.firebaseapp.com",
+  projectId:         "nexus-1f56f",
+  storageBucket:     "nexus-1f56f.firebasestorage.app",
   messagingSenderId: "925466332795",
-  appId: "1:925466332795:web:71cce1c5bb8f48d7459cde",
-  measurementId: "G-SR2WVCTLGS"
+  appId:             "1:925466332795:web:71cce1c5bb8f48d7459cde",
+  measurementId:     "G-SR2WVCTLGS",
 };
 
-const app  = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+initializeAppCheck(app, {
+  provider: new ReCaptchaEnterpriseProvider(
+    import.meta.env.VITE_RECAPTCHA_KEY
+  ),
+  isTokenAutoRefreshEnabled: true,
+});
+
 export const auth = getAuth(app);
-
 auth.languageCode = "es";
-
 auth.settings.appVerificationDisabledForTesting = false;
