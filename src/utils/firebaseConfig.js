310e8968c170d5c3834057394a525a22213b883d
leonel-1202 <wilfredo.silval@uniagustiniana.edu.co>
2026-05-28 02:18:22 -0500

    change

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyDeu4tFPDHNf44DpCgtvxGcqi3Urb0Wasc",
-  authDomain:        "nexus-web-a4123.firebaseapp.com",
-  projectId:         "nexus-web-a4123",
-  storageBucket:     "nexus-web-a4123.firebasestorage.app",
-  messagingSenderId: "1010374713756",
-  appId:             "1:1010374713756:web:471927c501d8cb84d32859",
+  apiKey: "AIzaSyAZdcW4g9apX8NtfG5EHSsRtwC4ogg0BEs",
+  authDomain: "nexus-1f56f.firebaseapp.com",
+  projectId: "nexus-1f56f",
+  storageBucket: "nexus-1f56f.firebasestorage.app",
+  messagingSenderId: "925466332795",
+  appId: "1:925466332795:web:71cce1c5bb8f48d7459cde",
+  measurementId: "G-SR2WVCTLGS"
 };
 
 const app  = initializeApp(firebaseConfig);
